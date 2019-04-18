21c3525256591f103df445bdf27bfd63eb369e73
Bonita Choi <bonitac93@gmail.com>
2019-04-18 01:07:24 +0000

    commiting sold out feature

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,15 @@ class ProductsController < ApplicationController
     @product = Product.find params[:id]
   end
 
+  def sold_out
+    # if @product.quantity.inspect == 0
+    #   puts "sold out"
+    # end
+    @soldout = Product.find params[:quantity]
+    if @soldout
+      
+    end
+    puts @sold_out
+  end
+
 end
