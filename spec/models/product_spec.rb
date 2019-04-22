a74e74b76743c7c95ad115d731215f80d21b4727
Bonita Choi <bonitac93@gmail.com>
2019-04-22 18:20:15 +0000

    added tests to check product validatiosn

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  before :each do
+    @category = Category.new(
+      name: "Category"
+    )
+    @product = Product.new(
+      name: 'Product',
+      price_cents: 500,
+      quantity: 10,
+      category: @category
+    )
+  end
+
+  describe 'Validations' do
+    it "is valid with valid attributes" do
+      expect(@product).to be_valid
+    end
+
+    it "is not valid without a name" do
+      @product.name = nil
+      expect(@product).to_not be_valid
+    end
+
+    it "is not valid without a price" do
+      @product.price_cents = nil
+      expect(@product).to_not be_valid
+    end
+
+    it "is not valid without a quantity" do
+      @product.quantity = nil
+      expect(@product).to_not be_valid
+    end
+
+    it "is not valid without a category" do
+      @product.category = nil
+      expect(@product).to_not be_valid
+    end
+
+  end
+end
